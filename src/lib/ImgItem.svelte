8df8087df103ca7d35ddefc64334e96c9d145041
adimar <adimaraisha@gmail.com>
2024-08-24 14:51:10 +0900

    photos grid

--- src/lib/ImgItem.svelte
+++ src/lib/ImgItem.svelte
@@ -8,12 +8,12 @@
 	};
 </script>
 
-<img on:click={toggle} src="/images/{ind}.jpeg" alt="" />
+<img class="w-full" on:click={toggle} src="/images/{ind}.jpeg" alt="" />
 
 {#if maximize}
 	<div
 		on:click={toggle}
-		class=" bg-black fixed top-0 left-0 w-screen h-screen z-50 flex flex-col items-center justify-center"
+		class="w-fulld bg-black fixed top-0 left-0 w-screen h-screen z-50 flex flex-col items-center justify-center"
 	>
 		<img class="w-screen" src="/images/{ind}.jpeg" alt="" />
 	</div>
--- src/lib/Photos.svelte
+++ src/lib/Photos.svelte
@@ -10,7 +10,7 @@
 	<button on:click={onBack} class="p-3 text-lg font-medium fixed top-0 left-0 w-full bg-white z-40"
 		>{'<-'} go back</button
 	>
-	<div class="grid grid-cols-2 flexd gap-1 flex-wrap justify-center">
+	<div class="grid grid-cols-2 sm:grid-cols-4 flexd gap-1 flex-wrap justify-center">
 		{#each imgCounts as img, ind}
 			<!-- <img class="max-w-32d" src="/images/{ind + 1}.jpeg" alt="" /> -->
 			<ImgItem ind={ind + 1} />
