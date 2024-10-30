4a707181ed56d349716dd853840a20473549c9ff
adimar <adimaraisha@gmail.com>
2024-10-30 19:57:40 +0800

    scan feature added

--- src/lib/MainPage.svelte
+++ src/lib/MainPage.svelte
@@ -1,30 +1,48 @@
-<script>
+<script lang="ts">
 	import Photos from './Photos.svelte';
 	import Sending from './Sending.svelte';
 	let showPhotos = false;
 	let isSending = false;
-
+	let type: 'sending' | 'scanning' = 'sending';
+	let hasResult = false;
 	const onSend = () => {
+		type = 'sending';
 		isSending = true;
 	};
-
 	const onAbort = () => {
 		isSending = false;
 	};
 </script>
 
 {#if isSending}
-	<Sending {onAbort} />
+	<Sending
+		{type}
+		{onAbort}
+		onSuccess={() => {
+			hasResult = true;
+		}}
+	/>
 {:else}
 	<h1 class="text-center text-xl p-6 font-bold text-indigo-500">Social Cracker</h1>
 	<main class="flex text-xl flex-col gap-3 justify-center items-center min-h-screen">
-		<button on:click={() => (showPhotos = true)} class="underline text-blue-600"
-			>see all photos</button
+		{#if hasResult}
+			<button on:click={() => (showPhotos = true)} class="underline text-blue-600"
+				>see all photos</button
+			>
+			<button on:click={onSend} class="btn">send to 100</button>
+			<button on:click={onSend} class="btn">send to 200</button>
+			<button on:click={onSend} class="btn">send to 500</button>
+			<button on:click={onSend} class="btn">send to everyone</button>
+		{/if}
+		<button
+			on:click={() => {
+				type = 'scanning';
+				isSending = true;
+			}}
+			class="bg-green-400 p-2 px-3 text-lg font-bold"
 		>
-		<button on:click={onSend} class="btn">send to 100</button>
-		<button on:click={onSend} class="btn">send to 200</button>
-		<button on:click={onSend} class="btn">send to 500</button>
-		<button on:click={onSend} class="btn">send to everyone</button>
+			Scan Local Area</button
+		>
 	</main>
 {/if}
 {#if showPhotos}
--- src/lib/Sending.svelte
+++ src/lib/Sending.svelte
@@ -1,12 +1,16 @@
 <script lang="ts">
 	import { onMount } from 'svelte';
 	export let onAbort: () => void;
+	export let type: 'sending' | 'scanning';
+	export let onSuccess: () => void;
+
 	let prog = 0;
 	let progAbort = 0;
 
 	let status: 'sending' | 'beingSent' | 'aborting' | 'aborted' = 'sending';
 
 	// let interval: typeof setInterval | undefined = undefined;
+
 	onMount(() => {
 		prog = 0;
 		progAbort = 0;
@@ -44,22 +48,47 @@
 			}
 		}, 25);
 	};
+
+	const onReturn = () => {
+		progAbort = 0;
+		status = 'beingSent';
+		onAbort();
+		onSuccess();
+	};
+
+	const abortText = () => (type == 'sending' ? 'Abort' : 'Cancel');
 </script>
 
 <main class="flex gap-6 flex-col items-center justify-center min-h-screen">
 	{#if status === 'beingSent'}
-		<p class="animate-pulse text-2xl text-green-600 font-bold">Photos are being sent...</p>
+		<p class=" text-2xl text-green-600 font-bold">
+			{#if type == 'sending'}
+				<span class="animate-pulse">Photos are being sent...</span>
+			{:else}
+				<span>already existing local domain found.</span>
+			{/if}
+		</p>
+		<button on:click={onReturn} class="underline text-md p-2 px-3 font-bold text-blue-600"
+			>Return</button
+		>
 	{:else if status === 'sending'}
-		<p class=" text-2xl text-green-600 font-bold">Sending Photos ... {prog} %</p>
+		<p class="animate-pulse text-2xl text-green-600 font-bold">
+			{type == 'sending' ? 'Sending Photos' : 'Scanning local domain'} ... {prog} %
+		</p>
+		<button on:click={aborting} class="text-xl font-bold bg-red-400 text-white p-3 px-6"
+			>{type == 'sending' ? 'abort' : 'cancel'}</button
+		>
 	{:else if status === 'aborting'}
-		<p class=" text-2xl text-orange-600 font-bold">Aborting ... {progAbort} %</p>
+		<p class=" text-2xl text-orange-600 font-bold">{abortText()} ... {progAbort} %</p>
 	{:else if status === 'aborted'}
-		<p class=" text-2xl text-green-600 font-bold">Progress Aborted</p>
+		<p class=" text-2xl text-green-600 font-bold">
+			{type == 'sending' ? 'Progress Aborted' : 'Scan Canceled'}
+		</p>
 	{/if}
 
-	{#if !status.includes('abort')}
+	<!-- {#if !status.includes('abort')}
 		<button on:click={aborting} class="text-xl font-bold bg-red-400 text-white p-3 px-6"
-			>abort</button
+			>{type == 'sending' ? 'abort' : 'cancel'}</button
 		>
-	{/if}
+	{/if} -->
 </main>
